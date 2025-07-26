c9517e3b833507ae7a94ab9c44f2f7329ede4615
N3-uchimura <bhfpy49728@yahoo.co.jp>
2025-07-26 13:27:45 +0900

    20250726

---
+++ src/consts/globalvariables.ts
@@ -0,0 +1,22 @@
+/**
+ * globalvariables.ts
+ **
+ * function：global variables
+**/
+
+/** const */
+// default
+export namespace myConst {
+  export const COMPANY_NAME: string = "nthree";
+  export const APP_NAME: string = "aozorastation";
+  export const LOG_LEVEL: string = "info";
+  export const DEFAULT_ENCODING: string = "utf8";
+  export const HOSTNAME: string = '127.0.0.1';
+}
+
+// default
+export namespace myNums {
+  export const WINDOW_WIDTH: number = 1200;
+  export const WINDOW_HEIGHT: number = 1000;
+  export const PORT: number = 5000;
+}
